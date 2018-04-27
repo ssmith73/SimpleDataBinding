baab7e22b97cb1724141ad37e4bca8acd8d3ed23
Sean Smith <ssmith73@gmail.com>
2018-04-27 18:37:35 +0100

    Initial Commit

---
+++ SimpleDataBinding/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDataBinding
+{
+    public class Person
+    {
+        private string name;
+        public Person()
+        {
+        }
+        public Person(string value)
+        {
+            this.name = value;
+        }
+        public string PersonName
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
+
